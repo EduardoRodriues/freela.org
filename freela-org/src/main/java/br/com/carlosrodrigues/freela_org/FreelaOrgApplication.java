c8bf98225d607a5bcc72b647dfd02d6583152ad2
Carlos Eduardo <eduardorodrigues@gmail.com>
2024-10-23 19:16:02 -0300

    inicio do projeto com maven

---
+++ freela-org/src/main/java/br/com/carlosrodrigues/freela_org/FreelaOrgApplication.java
@@ -0,0 +1,13 @@
+package br.com.carlosrodrigues.freela_org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelaOrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelaOrgApplication.class, args);
+	}
+
+}
