746443ee2da5719f1959280d81d25511d35dc7f7
Carlos Eduardo <eduardorodrigues@gmail.com>
2024-10-24 10:26:37 -0300

    adicionei o enum

---
+++ freela-org/src/main/java/br/com/carlosrodrigues/freela_org/core/enums/Icone.java
@@ -0,0 +1,18 @@
+package br.com.carlosrodrigues.freela_org.core.enums;
+
+public enum Icone {
+    
+    INICIANTE("Iniciante"),
+    INTERMEDIARIO("Intermediário"),
+    AVANCADO("Avançado");
+
+    private String nome;
+
+	private Icone(String nome) {
+        this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+    }
+}
