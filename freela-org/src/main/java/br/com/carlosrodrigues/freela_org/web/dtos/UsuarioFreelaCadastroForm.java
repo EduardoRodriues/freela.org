c324fff825e52eedc0ba711cb62507e8f7b5af14
Carlos Eduardo <eduardorodrigues@gmail.com>
2024-10-30 15:13:43 -0300

    formularios_de_usuario_adicionados

---
+++ freela-org/src/main/java/br/com/carlosrodrigues/freela_org/web/dtos/UsuarioFreelaCadastroForm.java
@@ -0,0 +1,63 @@
+package br.com.carlosrodrigues.freela_org.web.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioFreelaCadastroForm {
+
+    @Size(min = 3, max = 100)
+    @NotNull
+    private String nome;
+
+    @Email
+    @NotNull
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    private String senha;
+
+    @NotNull
+    @NotEmpty
+    private String confirmacaoSenha;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getConfirmacaoSenha() {
+		return confirmacaoSenha;
+	}
+
+	public void setConfirmacaoSenha(String confirmacaoSenha) {
+		this.confirmacaoSenha = confirmacaoSenha;
+	}
+}
